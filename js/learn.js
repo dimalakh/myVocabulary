a27adb10fea8e3215658235e91ca5830ee560b9f
dimalakh <dmytrolakh@gmail.com>
2017-04-04 22:39:40 +0300

    created some mockapp for learning page

--- js/learn.js
+++ js/learn.js
@@ -1,37 +1,42 @@
 import { StorageService } from './storage.service.js';  
 
 function init() {
-    let wordInpt = document.querySelector('#word-inpt');
-    let CheckBtn = document.querySelector('#check-btn');
+    let checkBtn = document.querySelector('#check-btn');
+    checkBtn.addEventListener('click', checkWord);
 
-    displayTask();
+    displayWord();
+
     chrome.runtime.getBackgroundPage(function(bg) {
     if (bg.entryToLoad)
       loadEntry(bg.entryToLoad);
   });
 }
 
-function createTask() {
-    let randomWord = {};
-    StorageService.get().then(arr => {
-        let randomNumber = getRandomInt(0, arr.length);
-        let tempArr = arr[randomNumber];
-        for(let prop in tempArr) {
-            Object.defineProperty(randomWord, prop, {
-                value: tempArr[prop]
-            });
-        }
+function setTaskWord() {
+    return StorageService.get().then(arr => {  
+             let randomNumber = getRandomInt(0, arr.length);
+             let randomWord = arr[randomNumber];
+             return randomWord;
+           });
+}
+
+function displayWord() {
+    setTaskWord().then(word => {
+        document.querySelector('#word-display').innerHTML = word.translation;
+        document.querySelector('#word-display').dataset.correct = word.name;
     });
-    return randomWord;
 }
 
-function displayTask() {
-    let word = createTask();
-    //document.querySelector('#word-display').innerHTML = word[translation];
-    console.log(word);
+function checkWord() {
+    let wordDisplay = document.querySelector('#word-display');
+    let checkInpt = document.querySelector('#check-input');
+     if(wordDisplay.dataset.correct === checkInpt.value) {
+         console.log('ok');
+         displayWord();
+     } else {
+         console.log('neok');
+     }
 }
-
-
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;

fc7024e6d7d8159400a6fd6ca630c7677fd4283e
dimalakh <dmytrolakh@gmail.com>
2017-04-10 11:06:45 +0300

    first version of extension

--- js/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if(message.page === "diction"){
-   chrome.tabs.create({url: chrome.extension.getURL('index.html')});
- }
- else if (message.page === "learn") {
-   chrome.tabs.create({url: chrome.extension.getURL('learn.html')});
- }
-});
--- js/word.js
+++
@@ -1,9 +0,0 @@
-export class Word {
-    constructor(wordName, translationValue) {
-        this.name = wordName;
-        this.translation = translationValue;
-        this.tries = 0;
-        this.correctAnswers = 0;
-        this.time = Date.now();
-    }
-}

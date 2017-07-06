9ce9a7ecd4cfff55ad7703c5bb367ff9ac22be7f
dimalakh <dmytrolakh@gmail.com>
2017-07-06 22:01:56 +0300

    rewrote background.js in ES6 style

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,8 @@
-"use strict";
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.page === "diction") {
-    chrome.tabs.create({ url: chrome.extension.getURL('index.html') });
-  } else if (message.page === "learn") {
-    chrome.tabs.create({ url: chrome.extension.getURL('learn.html') });
-  }
+/* eslint-disable no-undef */
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.page === 'diction')
+        chrome.tabs.create({ url: chrome.extension.getURL('index.html') });
+    if (message.page === 'learn')
+        chrome.tabs.create({ url: chrome.extension.getURL('learn.html') });
 });
+/* eslint-enable no-undef */

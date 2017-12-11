6e0073cdc8eaf80a534ac99a61520d4db66f484d
dimalakh <dmytrolakh@gmail.com>
2017-12-11 23:07:24 +0200

    Fixed config

--- src/js/helpers/auth.js
+++ src/js/helpers/auth.js
@@ -1,11 +1,5 @@
-require('dotenv').config()
+const config = require('../../../config')
 import firebase from 'firebase'
-
-const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL
-}
 
 firebase.initializeApp(config)
 
